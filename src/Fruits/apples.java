7d3648c0df067ba7be31fa63440cc21c4921b190
cosesdehacker <nasrodo@gmail.com>
2017-11-03 18:16:24 +0100

    aviam si ara ja sí

--- src/Fruits/apples.java
+++ src/Fruits/apples.java
@@ -2,9 +2,27 @@ package Fruits;
 
 import java.util.Scanner;
 
+import org.junit.Test;
+
+
 public class apples {
 
+
+    static String[] globalSphericalFruits;
+
+
     public static void main(String[] args) {
+
+        questions();
+        globalSphericalFruits = spherical();
+
+
+        thinkOver();
+
+
+    }
+
+    public static void questions() {
 
         Scanner sc = new Scanner(System.in);
 
@@ -12,9 +30,11 @@ public class apples {
                 "Apples are healthy and we would like you to fill this survey" +
                 " about apples.");
         System.out.println("What colour of apples do you prefer?");
+        System.out.println(" ");
         String colour = sc.next();
         System.out.println("I see... You prefer the " + colour + " ones...");
-        System.out.print("How many apples can you eat in a row?");
+        System.out.print("How many apples can you eat in a row? (type a number)");
+        System.out.println(" ");
 
         int numberapples = sc.nextInt();
 
@@ -25,42 +45,80 @@ public class apples {
         } else if (numberapples > 2 && numberapples <= 5) {
             System.out.println("Well... " + numberapples + " apples is definitely not bad.");
 
-        } else if (numberapples > 5){
+        } else if (numberapples > 5) {
             System.out.println(numberapples + "??? Really? You are an apple master!!");
         }
 
-        System.out.println ("Ok, I'll keep asking. If at any point you need to leave " +
-                "this survey, please type I'M FED UP WITH THIS SURVEY.");
-        System.out.println ("By, now, this is the next area we need to delve into.");
-        System.out.println ("Apples are spherical.");
-        System.out.println ("Tell me what other spherical fruits you can recall.");
-        System.out.println ("Type them then press enter after each.");
-        System.out.println ("When you can't think of more, type FINISH.");
-
-        //NO SURT, PERÒ HE D'ANAR A COMPRAR :P
-
-        /*while (sc.hasNext()) {
-
-        String s1=sc.next();
-
-        if (s1.equals("I'M FED UP WITH THIS SURVEY")) {
-            break; }
-
-            while (sc.hasNext()) {
-
-                String s2=sc.next();
-
-                if (s2.equals ("FINISH")) {
-                    break; }
-
-                System.out.println("baaaaah");}
-
-            System.out.println("Thank you. You have come up with some spherical fruits. ");
-            System.out.println("I would be proud of you but this doesn't actually prove anything ");
-            System.out.println("further than your intellectual age is above 2 years old. Congrats anyway."); }
-
-        System.out.println ("You ruined this fucking survey. I hope you have a horrible day!"); */
+        System.out.println("Ok, I'll keep asking. If at any point you need to leave " +
+                "this survey, please type BYE");
+        System.out.println("Are you ready to go on with it? Y/N");
 
     }
 
+    public static String[] spherical() {
+
+        String[] sphericalFruits = new String[11];
+        int counter = 0;
+        Scanner sc = new Scanner(System.in);
+        String s1;
+
+        String goon = sc.next();
+
+        if (goon.equalsIgnoreCase("Y")) {
+            System.out.println("Ok.");
+
+        } else {
+            System.out.println("I don't care, to be honest. I'll go on anyway.");
+            System.out.println("");
+        }
+
+        System.out.println("By, now, this is the next area we need to delve into.");
+        System.out.println("APPLES ARE SPHERICAL.");
+        System.out.println("Tell me what other spherical fruits you can recall.");
+        System.out.println("Type them then press enter after each.");
+        System.out.println("When you can't think of more, type FINISH.");
+
+
+        s1 = sc.next();
+
+        while (!(s1.equalsIgnoreCase("Bye") || (s1.equalsIgnoreCase("Finish")))) {
+            sphericalFruits[counter] = s1;
+            counter++;
+            System.out.print("");
+            s1 = sc.next();
+        }
+
+        if (s1.equalsIgnoreCase("BYE")) {
+            System.out.println("You ruined this fucking survey. I hope you have a horrible day!");
+        }
+
+        if (s1.equalsIgnoreCase("FINISH")) {
+
+            System.out.println("Thank you. You have come up with some spherical fruits. ");
+            System.out.println("I would be proud of you but this doesn't actually prove anything ");
+            System.out.println("further than your intellectual age is above 2 years old. Congrats anyway.");
+            System.out.println("");//HERE I WOULD LIKE THE PROGRAM TO RUN THE NEXT LINE ONLY WHE THE USER PRESSES ENTER
+        }
+        return sphericalFruits;
+
+    }
+
+    public static void thinkOver() { //PER QUÈ NO EL TROBA?? perquè el globalsphericalfruits és fora
+        // aquest mètode. l'hem posat a fora el main i l'hem convertit en static
+
+        for (int i = 0; i < globalSphericalFruits.length; i++) {
+            if (globalSphericalFruits[i] != null) {
+                System.out.println(globalSphericalFruits[i]);
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
---
+++ src/Fruits/applesTest.java
@@ -0,0 +1,25 @@
+package Fruits;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class applesTest {
+    @org.junit.Test
+    public void main() throws Exception {
+    }
+
+    @org.junit.Test
+    public void questions() throws Exception {
+    }
+
+    @org.junit.Test
+    public void spherical() throws Exception {
+    }
+
+    @org.junit.Test
+    public void thinkOver() throws Exception {
+    }
+
+
+}
